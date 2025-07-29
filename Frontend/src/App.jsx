7f4483c3531a6655725a648ae0d34a1414f709ad
AlbiMst <albimustakim61@gmail.com>
2025-07-29 19:36:23 +0700

    update frontend homepage, login, register, etc

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
-import ForgotPassword from "./pages/ForgotPassword";
-import ResetPassword from "./pages/ResetPassword";
 import Dashboard from "./pages/Dashboard";
 
 export default function App() {
@@ -12,8 +10,6 @@ export default function App() {
       <Routes>
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<HomePage />} />
       </Routes>
